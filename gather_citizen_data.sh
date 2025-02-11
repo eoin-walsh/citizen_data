24467ae600fdeb115f5bd1372798d0bbd5c3485d
Eoin Walsh <eoin.walsh@met.ie>
2025-02-11 13:52:52 +0000

    updated bash script

--- gather_citizen_data.sh
+++ gather_citizen_data.sh
@@ -7,13 +7,13 @@ Date="2024-10-28" #YYYY-MM-DD
 
 Directory="/home/ewalsh/Documents/projects/dublin_temps/grib" #directory address to the HECTOR grib files
 
-source ~/miniconda3/etc/profile.d/conda.sh
+source ~/miniforge3/etc/profile.d/conda.sh
 
-conda activate netatmo
+conda activate test
 
-# python netatmo_V2_EOIN.py $Date
+python netatmo_V2_EOIN.py $Date
 
-# python wundermap_data_acq.py $Date
+python wundermap_data_acq.py $Date
 
-python get_2m_temp_field.py $Date $Directory
+python gridpp_v1.py $Date $Directory
 
